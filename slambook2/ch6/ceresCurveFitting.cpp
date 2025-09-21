e2578d0a40338d5c097a359a3d52ce84177e352f
congyuxiaoyoudao <apeiria0101@gmail.com>
2025-09-22 00:23:20 +0800

    [ch6]: Walk through non-linear optimation methods

--- slambook2/ch6/ceresCurveFitting.cpp
+++ slambook2/ch6/ceresCurveFitting.cpp
@@ -25,30 +25,53 @@ struct CURVE_FITTING_COST {
   const double _x, _y;    // x,y数据
 };
 
+// Another curve fitting cost function
+// y=ae^{bx}+csin(dx+e)+fx
+// so here this function is a strongly non-linear function,
+// if initialized badly, the solution may not converge,
+// or you may see it need more iterations to converge
+struct CURVE_FITTING_COST2 {
+  CURVE_FITTING_COST2(double x, double y) : _x(x), _y(y) {}
+
+  // 残差的计算
+  template<typename T>
+  bool operator()(
+    const T *const abcdef, // 6 dimension params
+    T *residual) const {
+    residual[0] = T(_y) - abcdef[0]*ceres::exp(abcdef[1]*T(_x))-abcdef[2]*ceres::sin(abcdef[3]*T(_x)+abcdef[4]) \
+                  -abcdef[5]*T(_x); // y-ae^{bx}-csin(dx+e)-fx
+    return true;
+  }
+
+  const double _x, _y;
+};
+
 int main(int argc, char **argv) {
-  double ar = 1.0, br = 2.0, cr = 1.0;         // 真实参数值
-  double ae = 2.0, be = -1.0, ce = 5.0;        // 估计参数值
+  double ar = 1.0, br = 2.0, cr = 1.0, dr = 2.0, er = 3.0, fr = 4.0;         // 真实参数值
+  double ae = 1.2, be = 1.0, ce = 0.5, de = 2.5, ee = 3.0, fe = 3.0;         // 估计参数值
   int N = 100;                                 // 数据点
   double w_sigma = 1.0;                        // 噪声Sigma值
   double inv_sigma = 1.0 / w_sigma;
   cv::RNG rng;                                 // OpenCV随机数产生器
 
-  vector<double> x_data, y_data;      // 数据
+  vector<double> x_data, y1_data, y2_data;      // 数据
   for (int i = 0; i < N; i++) {
     double x = i / 100.0;
     x_data.push_back(x);
-    y_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
+    y1_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
+    y2_data.push_back(ar * exp(br * x) + cr * sin(dr * x + er) + fr * x + rng.gaussian(w_sigma * w_sigma));
   }
 
   double abc[3] = {ae, be, ce};
+  double abcdef[6] = {ae, be, ce, de, ee, fe};
 
   // 构建最小二乘问题
-  ceres::Problem problem;
+  ceres::Problem problem1;
   for (int i = 0; i < N; i++) {
-    problem.AddResidualBlock(     // 向问题中添加误差项
+    problem1.AddResidualBlock(     // 向问题中添加误差项
       // 使用自动求导，模板参数：误差类型，输出维度，输入维度，维数要与前面struct中一致
       new ceres::AutoDiffCostFunction<CURVE_FITTING_COST, 1, 3>(
-        new CURVE_FITTING_COST(x_data[i], y_data[i])
+        new CURVE_FITTING_COST(x_data[i], y1_data[i])
       ),
       nullptr,            // 核函数，这里不使用，为空
       abc                 // 待估计参数
@@ -56,21 +79,50 @@ int main(int argc, char **argv) {
   }
 
   // 配置求解器
-  ceres::Solver::Options options;     // 这里有很多配置项可以填
-  options.linear_solver_type = ceres::DENSE_NORMAL_CHOLESKY;  // 增量方程如何求解
-  options.minimizer_progress_to_stdout = true;   // 输出到cout
+  ceres::Solver::Options options1;     // 这里有很多配置项可以填
+  options1.linear_solver_type = ceres::DENSE_NORMAL_CHOLESKY;  // 增量方程如何求解
+  options1.minimizer_progress_to_stdout = true;   // 输出到cout
 
-  ceres::Solver::Summary summary;                // 优化信息
+  ceres::Solver::Summary summary1;                // 优化信息
   chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
-  ceres::Solve(options, &problem, &summary);  // 开始优化
+  ceres::Solve(options1, &problem1, &summary1);  // 开始优化
   chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
   chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
   cout << "solve time cost = " << time_used.count() << " seconds. " << endl;
 
   // 输出结果
-  cout << summary.BriefReport() << endl;
-  cout << "estimated a,b,c = ";
-  for (auto a:abc) cout << a << " ";
+  cout << summary1.BriefReport() << endl; // CONVERGENCE over 17 iters
+  cout << "estimated a,b,c = ";       // GT 1.0     2.0     1.0 
+  for (auto a:abc) cout << a << " ";  // ES 1.05489 1.92158 1.02832
+  cout << endl;
+
+  cout << "=====================================================================================" << endl;
+  ceres::Problem problem2;
+  for (int i = 0; i < N; i++) {
+    problem2.AddResidualBlock(     // 向问题中添加误差项
+      // 使用自动求导，模板参数：误差类型，输出维度，输入维度，维数要与前面struct中一致
+      new ceres::AutoDiffCostFunction<CURVE_FITTING_COST2, 1, 6>(
+        new CURVE_FITTING_COST2(x_data[i], y2_data[i])
+      ),
+      nullptr,            // 核函数，这里不使用，为空
+      abcdef                 // 待估计参数
+    );
+  }
+
+  ceres::Solver::Options options2;  // use some different options
+  options2.trust_region_strategy_type = ceres::DOGLEG;  // DogLeg method
+  options2.minimizer_progress_to_stdout = true;
+
+  ceres::Solver::Summary summary2;
+  t1 = chrono::steady_clock::now();
+  ceres::Solve(options2, &problem2, &summary2);
+  t2 = chrono::steady_clock::now();
+  time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+  cout << "solve time cost = " << time_used.count() << " seconds. " << endl;
+
+  cout << summary2.BriefReport() << endl; // CONVERGENCE over 24 iters
+  cout << "estimated a,b,c,d,e,f = ";   // GT 1.0      2.0     1.0       2.0     3.0     4.0
+  for (auto a:abcdef) cout << a << " "; // ES 0.522288 2.50634 -0.365476 6.14024 3.42716 4.3175
   cout << endl;
 
   return 0;
--- slambook2/ch6/g2oCurveFitting.cpp
+++ slambook2/ch6/g2oCurveFitting.cpp
@@ -67,24 +67,89 @@ public:
   double _x;  // x 值， y 值为 _measurement
 };
 
+typedef Eigen::Matrix<double, 6, 1> Vector6d;
+
+class CurveFittingVertex2 : public g2o::BaseVertex<6, Vector6d> {
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  // 重置
+  virtual void setToOriginImpl() override {
+    _estimate << 0, 0, 0, 0, 0, 0;
+  }
+
+  // 更新
+  virtual void oplusImpl(const double *update) override {
+    _estimate += Vector6d(update);
+  }
+
+  // 存盘和读盘：留空
+  virtual bool read(istream &in) {}
+
+  virtual bool write(ostream &out) const {}
+};
+
+class CurveFittingEdge2 : public g2o::BaseUnaryEdge<1, double, CurveFittingVertex2> {
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  CurveFittingEdge2(double x) : BaseUnaryEdge(), _x(x) {}
+
+  // 计算曲线模型误差
+  virtual void computeError() override {
+    const CurveFittingVertex2 *v = static_cast<const CurveFittingVertex2 *> (_vertices[0]);
+    const Vector6d abcdef = v->estimate();
+    _error(0, 0) = _measurement - abcdef(0,0) * std::exp(abcdef(1, 0) * _x) - \
+                    abcdef(2,0)*std::sin(abcdef(3,0)*_x+abcdef(4,0)) - abcdef(5,0)*_x;
+  }
+
+  // 计算雅可比矩阵
+  virtual void linearizeOplus() override {
+    const CurveFittingVertex2 *v = static_cast<const CurveFittingVertex2 *> (_vertices[0]);
+    const Vector6d abcdef = v->estimate();
+    double y = abcdef(0,0) * std::exp(abcdef(1, 0) * _x) + \
+                abcdef(2,0)*std::sin(abcdef(3,0)*_x+abcdef(4,0)) + abcdef(5,0)*_x;
+    
+                double exp_term = exp(abcdef(1)*_x);
+    double sin_term = sin(abcdef(3)*_x + abcdef(4));
+
+    _jacobianOplusXi[0] = -exp_term;
+    _jacobianOplusXi[1] = -abcdef(0) * _x * exp_term;
+    _jacobianOplusXi[2] = -sin_term;
+    _jacobianOplusXi[3] = -abcdef(2) * _x * cos(abcdef(3)*_x + abcdef(4));
+    _jacobianOplusXi[4] = -abcdef(2) * cos(abcdef(3)*_x + abcdef(4));
+    _jacobianOplusXi[5] = -_x;
+  }
+
+  virtual bool read(istream &in) {}
+
+  virtual bool write(ostream &out) const {}
+
+public:
+  double _x;  // x 值， y 值为 _measurement
+};
+
 int main(int argc, char **argv) {
-  double ar = 1.0, br = 2.0, cr = 1.0;         // 真实参数值
-  double ae = 2.0, be = -1.0, ce = 5.0;        // 估计参数值
+  double ar = 1.0, br = 2.0, cr = 1.0, dr = 2.0, er = 3.0, fr = 4.0;         // 真实参数值
+  double ae = 1.2, be = 1.0, ce = 0.5, de = 2.5, ee = 3.0, fe = 3.0;         // 估计参数值
   int N = 100;                                 // 数据点
   double w_sigma = 1.0;                        // 噪声Sigma值
   double inv_sigma = 1.0 / w_sigma;
   cv::RNG rng;                                 // OpenCV随机数产生器
 
-  vector<double> x_data, y_data;      // 数据
+  vector<double> x_data, y1_data, y2_data;      // 数据
   for (int i = 0; i < N; i++) {
     double x = i / 100.0;
     x_data.push_back(x);
-    y_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
+    y1_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
+    y2_data.push_back(ar*exp(br*x)+cr*sin(dr*x+er)+fr*x+rng.gaussian(w_sigma*w_sigma));
   }
 
   // 构建图优化，先设定g2o
   typedef g2o::BlockSolver<g2o::BlockSolverTraits<3, 1>> BlockSolverType;  // 每个误差项优化变量维度为3，误差值维度为1
+  typedef g2o::BlockSolver<g2o::BlockSolverTraits<6, 1>> BlockSolverType2;  // 每个误差项优化变量维度为6，误差值维度为1
   typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType; // 线性求解器类型
+  typedef g2o::LinearSolverDense<BlockSolverType2::PoseMatrixType> LinearSolverType2;
 
   // 梯度下降方法，可以从GN, LM, DogLeg 中选
   auto solver = new g2o::OptimizationAlgorithmGaussNewton(
@@ -104,7 +169,7 @@ int main(int argc, char **argv) {
     CurveFittingEdge *edge = new CurveFittingEdge(x_data[i]);
     edge->setId(i);
     edge->setVertex(0, v);                // 设置连接的顶点
-    edge->setMeasurement(y_data[i]);      // 观测数值
+    edge->setMeasurement(y1_data[i]);      // 观测数值
     edge->setInformation(Eigen::Matrix<double, 1, 1>::Identity() * 1 / (w_sigma * w_sigma)); // 信息矩阵：协方差矩阵之逆
     optimizer.addEdge(edge);
   }
@@ -121,6 +186,43 @@ int main(int argc, char **argv) {
   // 输出优化值
   Eigen::Vector3d abc_estimate = v->estimate();
   cout << "estimated model: " << abc_estimate.transpose() << endl;
+
+  cout << "=====================================================================================" << endl;
+  
+  // Now use another method
+  auto solver2 = new g2o::OptimizationAlgorithmDogleg(
+    g2o::make_unique<BlockSolverType2>(g2o::make_unique<LinearSolverType2>()));
+  g2o::SparseOptimizer optimizer2;     // 图模型
+  optimizer2.setAlgorithm(solver2);   // 设置求解器
+  optimizer2.setVerbose(true); 
+
+  CurveFittingVertex2 *v2 = new CurveFittingVertex2();
+  Vector6d est;
+  est << ae, be, ce, de, ee, fe;
+  v2->setEstimate(est);
+  v2->setId(0);
+  optimizer2.addVertex(v2);
+
+  for (int i = 0; i < N; i++) {
+    CurveFittingEdge2 *edge = new CurveFittingEdge2(x_data[i]);
+    edge->setId(i);
+    edge->setVertex(0, v2);                // 设置连接的顶点
+    edge->setMeasurement(y2_data[i]);      // 观测数值
+    edge->setInformation(Eigen::Matrix<double, 1, 1>::Identity() * 1 / (w_sigma * w_sigma)); // 信息矩阵：协方差矩阵之逆
+    optimizer2.addEdge(edge);
+  }
+
+  cout << "start optimization for another strongly non-linear problem" << endl;
+  t1 = chrono::steady_clock::now();
+  optimizer2.initializeOptimization();
+  optimizer2.optimize(20);
+  t2 = chrono::steady_clock::now();
+  time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+  cout << "solve time cost = " << time_used.count() << " seconds. " << endl;
+
+  // 输出优化值
+  Vector6d abcdef_estimate = v2->estimate();
+  cout << "estimated model: " << abcdef_estimate.transpose() << endl;
 
   return 0;
 }
